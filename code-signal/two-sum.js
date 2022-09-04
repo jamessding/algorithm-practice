bc38e32471ca0ce0a4ba3126856365b237c35e8a
James Ding <jamesding528@yahoo.com>
2022-09-04 01:26:10 +0000

    added optimal solution of two-sum

--- code-signal/two-sum.js
+++ code-signal/two-sum.js
@@ -25,12 +25,25 @@
 //   - 109 <= target <= 109
 // Only one valid answer exists.
 
+// initial solution Time: O(n^2)
 var twoSum = function (nums, target) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length; j++) {
       if (i !== j && nums[i] + nums[j] === target) {
         return [i, j];
       }
+    }
+  }
+};
+
+// optimal Time: solution O(n) Mem: O(n)
+var twoSum = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
     }
   }
 };
